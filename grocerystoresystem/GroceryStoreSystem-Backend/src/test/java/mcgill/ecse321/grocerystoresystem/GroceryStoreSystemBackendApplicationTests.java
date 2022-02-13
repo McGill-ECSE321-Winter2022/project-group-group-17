63466a41509e4e417dc3f568aee59e1bf5fb5081
Kachi-Ezekwem <onyekachi.ezekwem@mail.mcgill.ca>
2022-02-13 11:11:56 -0500

    Initial commit of the backend application

---
+++ grocerystoresystem/GroceryStoreSystem-Backend/src/test/java/mcgill/ecse321/grocerystoresystem/GroceryStoreSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mcgill.ecse321.grocerystoresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
