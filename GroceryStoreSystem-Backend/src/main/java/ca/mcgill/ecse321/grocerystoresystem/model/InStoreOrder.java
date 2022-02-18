c85ce5941bab65f5032180526a58da106eed5e64
Yash Khapre <yash.khapre@mail.mcgill.ca>
2022-02-18 16:29:17 -0500

    Update InStoreOrder.java

--- GroceryStoreSystem-Backend/src/main/java/ca/mcgill/ecse321/grocerystoresystem/model/InStoreOrder.java
+++ GroceryStoreSystem-Backend/src/main/java/ca/mcgill/ecse321/grocerystoresystem/model/InStoreOrder.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 public class InStoreOrder extends Order {
     
-    public DeliveryOrder(int totalCost, LocalDateTime orderTimeStamp, boolean isPaid, String orderID) {
+    public InStoreOrder(int totalCost, LocalDateTime orderTimeStamp, boolean isPaid, String orderID) {
         super(totalCost, orderTimeStamp, isPaid, orderID);
     }
 
