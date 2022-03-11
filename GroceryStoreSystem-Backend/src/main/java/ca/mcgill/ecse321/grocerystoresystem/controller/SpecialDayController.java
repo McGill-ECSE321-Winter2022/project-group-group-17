be262b5b28837cb5763cc0d216298c791af307b3
Yash Khapre <yash.khapre@mail.mcgill.ca>
2022-03-10 20:27:01 -0500

    Update SpecialDayController.java
    Added JavaDoc to controller files

--- GroceryStoreSystem-Backend/src/main/java/ca/mcgill/ecse321/grocerystoresystem/controller/SpecialDayController.java
+++ GroceryStoreSystem-Backend/src/main/java/ca/mcgill/ecse321/grocerystoresystem/controller/SpecialDayController.java
@@ -28,69 +28,80 @@ public class SpecialDayController {
   @Autowired
   private SpecialDayService specialDayService;
   
+  /**
+   * @author Yash Khapre
+   * @param calendarID
+   * Controller method to get closed days
+   */
   @GetMapping(value = { "/closeddays/{id}", "/closeddays/{id}/" })
   public ResponseEntity getClosedDays(@PathVariable("id") int calendarID) {
     List<SpecialDayDto> closedDaysDto = new ArrayList<>();
-    List<SpecialDay> closedDays;
-    
+    List<SpecialDay> closedDays;    
     try {
       closedDays = specialDayService.getClosedDays(calendarID);
   } catch (IllegalArgumentException exception) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-  }
-  
+  }  
     for(SpecialDay sDay : closedDays) {
       closedDaysDto.add(convertToDto(sDay));
-    }
-    
+    }    
     if (closedDaysDto.isEmpty()) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No closedDays with specified calendar ID exist");
     }
     return new ResponseEntity<>(closedDaysDto, HttpStatus.OK);
   }
   
+  /**
+   * @author Yash Khapre
+   * @param specialDayID
+   * Controller method to get all shifts on a specified special day
+   */
   @GetMapping(value = { "/shifts/{id}", "/shifts/{id}/" })
   public ResponseEntity getSpecialShifts(@PathVariable("id") int specialDayID) {
     List<ShiftDto> specialShiftsDtoList = new ArrayList<>();
-    List<Shift> specialShifts;
-    
+    List<Shift> specialShifts;    
     try {
       specialShifts = specialDayService.getSpecialShifts(specialDayID);
     } catch(IllegalArgumentException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-    }
-    
+    }    
     for(Shift s : specialShifts) {
       specialShiftsDtoList.add(convertToDto(s));
-    }
-    
+    }    
     if(specialShiftsDtoList.isEmpty()) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No shifts at specified Special Day exist");
     }
     return new ResponseEntity<>(specialShiftsDtoList, HttpStatus.OK);    
   }
   
+  /**
+   * @author Yash Khapre
+   * @param specialDayID
+   * Controller method that gets the employees on a specified specialDay
+   */
   @GetMapping(value = { "/employees/{id}", "/employees/{id}/" })
   public ResponseEntity getEmployeesOnSpecialShifts(@PathVariable("id") int specialDayID) {
     List<EmployeeDto> employeeDtoList = new ArrayList<>();
-    List<Employee> employeeList;
-    
+    List<Employee> employeeList;    
     try {
       employeeList = specialDayService.getEmployeesOnSpecialShifts(specialDayID);
     } catch(IllegalArgumentException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-    }
-    
+    }    
     for(Employee e : employeeList) {
       employeeDtoList.add(convertToDto(e));
-    }
-    
+    }    
     if(employeeList.isEmpty()) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No employees assigned at specified Special Day!");
     }
     return new ResponseEntity<>(employeeDtoList, HttpStatus.OK);    
   }
   
+  /**
+   * @author Yash Khaprre
+   * @param specialDayID, startTime, endTime
+   * Controller method to update a specialDay's features
+   */
   @PutMapping(value = { "/specialday/update/{id}", "/specialday/update/{id}/" })
   public ResponseEntity updateSpecialDay(@PathVariable("id") int specialDayID, LocalDateTime startTime, LocalDateTime endTime) {
     SpecialDay sDay;
@@ -105,6 +116,11 @@ public class SpecialDayController {
     return new ResponseEntity<>(convertToDto(sDay), HttpStatus.OK);
   }
   
+  /**
+   * @author Yash Khapre
+   * @param specialDayID, shiftID, date, startTime, endTime, shiftStatus, personID
+   * Update a specific Shift on a specific specialDay
+   */
   @PutMapping(value = { "/shift/update/{id}", "/shift/update/{id}/" })
   public ResponseEntity updateSpecificDayShift(@PathVariable("id") int specialDayID, int shiftID, LocalDate date, 
       LocalTime startTime, LocalTime endTime, ShiftStatus shiftStatus, int personID) {
@@ -120,8 +136,13 @@ public class SpecialDayController {
     return new ResponseEntity<>(convertToDto(shift), HttpStatus.OK);
   }
   
+  /**
+   * @author Yash Khapre
+   * @param specialDayID
+   * Controller method to delete a specific specialDay
+   */
   @DeleteMapping(value = { "/specialDay/{id}", "/specialDay/{id}/" })
-  public ResponseEntity deleteCustomer(@PathVariable("id") int specialDayID){
+  public ResponseEntity deleteSpecialDay(@PathVariable("id") int specialDayID){
       boolean delete;
       try {
           delete = specialDayService.deleteSpecialDayByID(specialDayID);
@@ -134,6 +155,11 @@ public class SpecialDayController {
       return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("Error deleting special day!");
   }
   
+  /**
+   * @author Yash Khapre
+   * @param SpecialDay sDay
+   * Method to convert a SpecialDay into a SpecialDayDto
+   */
   private SpecialDayDto convertToDto(SpecialDay sDay) {
     if (sDay == null) {
       throw new NullPointerException("Cannot find this Special Day!");
@@ -142,6 +168,11 @@ public class SpecialDayController {
     return new SpecialDayDto(sDay.getStartTimestamp(), sDay.getEndTimestamp(), sDay.getSpecialdayID());
     }
   
+  /**
+   * @author Yash Khapre
+   * @param Shift s
+   * Method to convert a Shift into a ShiftDto
+   */
   private ShiftDto convertToDto(Shift s) {
     if (s == null) {
       throw new NullPointerException("Cannot find this Shift!");
@@ -149,6 +180,11 @@ public class SpecialDayController {
     return new ShiftDto(s.getShiftID(), s.getDate(), s.getStartTime(), s.getEndTime(), s.getShiftStatus(), convertToDto(s.getEmployee()));
     }
   
+  /**
+   * @author Yash Khapre
+   * @param Employee e
+   * Method to convert an Employee into an EmployeeDto
+   */
   private EmployeeDto convertToDto(Employee e) {
     if (e == null) {
       throw new NullPointerException("Cannot find this Employee!");
@@ -156,6 +192,11 @@ public class SpecialDayController {
     return new EmployeeDto(e.getPersonID(), e.getFirstName(), e.getLastName(), e.getEmail(), e.getEmpStatus(), convertToDto(e.getAddress()));
     }
   
+  /**
+   * @author Yash Khapre
+   * @param Address address
+   * Method to convert an Address into an AddressDto
+   */
   private AddressDto convertToDto(Address address) {
     if (address == null) {
       throw new NullPointerException("Cannot find Address!");
