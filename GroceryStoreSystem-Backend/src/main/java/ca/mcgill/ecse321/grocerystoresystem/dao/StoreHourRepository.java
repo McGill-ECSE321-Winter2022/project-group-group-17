5a60b17a0c8bddc9cf99d02ddf8070106e8c475f
Amy Qi Wang <amy.wang@mail.mcgill.ca>
2022-03-17 15:04:38 -0400

    fixing storehour repo

--- GroceryStoreSystem-Backend/src/main/java/ca/mcgill/ecse321/grocerystoresystem/dao/StoreHourRepository.java
+++ GroceryStoreSystem-Backend/src/main/java/ca/mcgill/ecse321/grocerystoresystem/dao/StoreHourRepository.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse321.grocerystoresystem.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.grocerystoresystem.model.StoreHour;
+import ca.mcgill.ecse321.grocerystoresystem.model.Weekdays;
 
 public interface StoreHourRepository extends CrudRepository<StoreHour, Integer >{
 
@@ -15,4 +16,9 @@ public interface StoreHourRepository extends CrudRepository<StoreHour, Integer >
 	 * Returns true if their is a Store Hour in the database with the passed ID as parameter
 	 */
 	boolean existsByStoreHourID(Integer storeHourID);
+
+		/**
+	 * Find StoreHour based on weekday
+	 */
+	StoreHour findStoreHourByWeekday (Weekdays day);
 }
