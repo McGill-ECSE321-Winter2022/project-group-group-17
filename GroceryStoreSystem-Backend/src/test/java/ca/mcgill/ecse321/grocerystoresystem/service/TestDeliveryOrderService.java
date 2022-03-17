ee81771362ce7612f41e92b6d80f2e5912e460fe
Kachi-Ezekwem <onyekachi.ezekwem@mail.mcgill.ca>
2022-03-16 22:30:56 -0400

    Completed the DeliveryOrderService Tests

--- GroceryStoreSystem-Backend/src/test/java/ca/mcgill/ecse321/grocerystoresystem/service/TestDeliveryOrderService.java
+++ GroceryStoreSystem-Backend/src/test/java/ca/mcgill/ecse321/grocerystoresystem/service/TestDeliveryOrderService.java
@@ -9,6 +9,8 @@ import ca.mcgill.ecse321.grocerystoresystem.model.DeliveryOrder;
 import ca.mcgill.ecse321.grocerystoresystem.dao.AddressRepository;
 import ca.mcgill.ecse321.grocerystoresystem.dao.DeliveryOrderRepository;
 
+import ca.mcgill.ecse321.grocerystoresystem.model.Owner;
+import ca.mcgill.ecse321.grocerystoresystem.model.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -42,7 +44,19 @@ public class TestDeliveryOrderService {
 	private static final LocalDateTime ORDER_TIME_STAMP = LocalDateTime.of(2022, 01,01,01,01,01,01);
 	private static final boolean IS_PAID = true;
 	private static final LocalDateTime DELIVERY_TIME = LocalDateTime.of(2022,01,02,01,01,01,01);
-	
+
+	private static final int ORDER_ID2 = 1000;
+	private static final int TOTAL_COST2 = 1234;
+	private static final LocalDateTime ORDER_TIME_STAMP2 = LocalDateTime.of(2022, 02,01,01,01,01,01);
+	private static final boolean IS_PAID2 = true;
+	private static final LocalDateTime DELIVERY_TIME2 = LocalDateTime.of(2022,02,02,01,01,01,01);
+
+	private static final int ORDER_ID3 = 1111;
+	private static final int TOTAL_COST3 = 3404;
+	private static final LocalDateTime ORDER_TIME_STAMP3 = LocalDateTime.of(2022, 03,01,01,01,01,01);
+	private static final boolean IS_PAID3 = true;
+	private static final LocalDateTime DELIVERY_TIME3 = LocalDateTime.of(2022,03,02,01,01,01,01);
+
 	private static final int ADDRESS_KEY = 1001;
     private static final String STREET_NUM = "1239";
     private static final String STREET_NAME = "Peel";
@@ -50,6 +64,9 @@ public class TestDeliveryOrderService {
     private static final String POSTAL_CODE = "HHH HHH";
     private static final String COUNTRY = "Canada";
     private static final boolean IS_LOCAL = true;
+
+	private static final int PERSON_KEY = 1002;
+
 	
 	@BeforeEach
 	public void setMockOutput() {
@@ -61,7 +78,29 @@ public class TestDeliveryOrderService {
 	                return null;
 	            }
 	        });
-		 
+
+		 lenient().when(deliveryOrderRepository.findDeliveryOrderByDeliveryAddressAddressID(anyInt()))
+				 .thenAnswer((InvocationOnMock invocation) -> {
+					 if(invocation.getArgument(0).equals(ADDRESS_KEY)) {
+					 	List<DeliveryOrder> orders = createDeliveryOrders();
+					 	return orders;
+					 } else {
+					 	return new ArrayList<>();
+					 }
+
+				 });
+
+		lenient().when(deliveryOrderRepository.findDeliveryOrderByPersonPersonID(anyInt()))
+				.thenAnswer((InvocationOnMock invocation) -> {
+					if(invocation.getArgument(0).equals(PERSON_KEY)) {
+						List<DeliveryOrder> orders = createDeliveryOrders2();
+						return orders;
+					} else {
+						return new ArrayList<>();
+					}
+
+				});
+
 		 lenient().when(addressRepository.findAddressByAddressID(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
 	            if(invocation.getArgument(0).equals(ADDRESS_KEY)) {
 	                return createAddress();
@@ -141,6 +180,26 @@ public class TestDeliveryOrderService {
 		 assertEquals("Please submit a valid totalCost", error);
 	 }
 
+	 @Test
+	 public void successfullyGetDeliveryOrdersWithPerson(){
+		 List<DeliveryOrder> savedOrder = null;
+
+		 savedOrder = deliveryOrderService.getAllDeliveryOrdersOfPersonWithPersonID(PERSON_KEY);
+
+		 assertNotNull(savedOrder);
+		 assertEquals(1, savedOrder.size());
+	 }
+
+	 @Test
+	 public void successfullyGetDeliveryOrdersWithAddress(){
+		List<DeliveryOrder> savedOrder = null;
+
+		savedOrder = deliveryOrderService.getDeliveryOrdersForAddressWithAddressID(ADDRESS_KEY);
+
+		assertNotNull(savedOrder);
+		assertEquals(2, savedOrder.size());
+	 }
+
 	@Test
 	public void successfullyGetDeliveryOrderByID() {
 		DeliveryOrder deliveryOrder = null;
@@ -167,13 +226,6 @@ public class TestDeliveryOrderService {
 		deliveryOrder.setDeliveryTime(DELIVERY_TIME);
 		return deliveryOrder;
 	}
-	
-	private DeliveryOrder createDeliveryOrderWithAddress() {
-		DeliveryOrder deliveryOrder = createDeliveryOrder();
-		Address address = createAddress();
-		deliveryOrder.setAddress(address);
-		return deliveryOrder;
-	}
 		
 		
 	
@@ -190,8 +242,44 @@ public class TestDeliveryOrderService {
 		
 	}
 	
-	
-	
-	
+	private List<DeliveryOrder> createDeliveryOrders(){
+		ArrayList<DeliveryOrder> orders = new ArrayList<>();
+
+		DeliveryOrder order = new DeliveryOrder();
+		order.setOrderID(ORDER_ID);
+		order.setTotalCost(TOTAL_COST);
+		order.setOrderTimeStamp(ORDER_TIME_STAMP);
+		order.setPaid(IS_PAID);
+		order.setDeliveryTime(DELIVERY_TIME);
+
+		DeliveryOrder order2 = new DeliveryOrder();
+		order2.setOrderID(ORDER_ID2);
+		order2.setTotalCost(TOTAL_COST2);
+		order2.setOrderTimeStamp(ORDER_TIME_STAMP2);
+		order2.setPaid(IS_PAID2);
+		order2.setDeliveryTime(DELIVERY_TIME2);
+
+		orders.add(order);
+		orders.add(order2);
+
+		return orders;
+	}
+
+
+	private List<DeliveryOrder> createDeliveryOrders2(){
+		ArrayList<DeliveryOrder> orders = new ArrayList<>();
+
+		DeliveryOrder order = new DeliveryOrder();
+		order.setOrderID(ORDER_ID3);
+		order.setTotalCost(TOTAL_COST3);
+		order.setOrderTimeStamp(ORDER_TIME_STAMP3);
+		order.setPaid(IS_PAID3);
+		order.setDeliveryTime(DELIVERY_TIME3);
+
+		orders.add(order);
+
+		return orders;
+	}
+
 
 }
