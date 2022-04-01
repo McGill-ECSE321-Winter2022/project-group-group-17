cb91386ee8741f38b538a9f750bdf579e0222061
TinetendoWamambo <tinetendo.makata@mail.mcgill.ca>
2022-04-01 16:46:30 -0400

    working

--- GroceryStoreSystem-Frontend/src/tryingVueJS/app.js
+++ GroceryStoreSystem-Frontend/src/tryingVueJS/app.js
@@ -1,6 +1,13 @@
+
 const app = Vue.createApp({
-  data(){
-  return{
-  firstName: 'John'}}
+	template:  '<h1> {{storeName}}</h1>',
+	data(){
+		return{
+			storeName: 'Hops Grocery',
+			picture: 'https://i.pinimg.com/564x/d4/9c/6d/d49c6d145d811eb24e976567687576cf.jpg',
+			
+		}
+	}
 })
+
 app.mount('#app')
