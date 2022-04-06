8af8f6da66c774bb997d4e4a042c36f0f67f5f00
ruiding-code <rui.du.2000@gmail.com>
2022-04-05 22:57:16 -0400

    Frontend for orders and order history

--- GroceryStoreSystem-Frontend/src/router/index.js
+++ GroceryStoreSystem-Frontend/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Hello from '@/components/Hello'
+import Order from '@/components/Order'
+import ViewPersonalOrders from '@/components/ViewPersonalOrders'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/myorders',
+      name: 'PersonalOrders',
+      component: ViewPersonalOrders
+    },
+    {
+      path: '/myorders/2',
+      name: 'order',
+      component: Order
     }
   ]
 })
